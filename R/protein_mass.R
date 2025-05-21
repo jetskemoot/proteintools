37454bf0e857cdb8b56ea4a2f9f3ece53aaa507f
jetskemoot <jetskemoot@users.noreply.github.com>
2025-05-21 17:48:44 +0200

    new function protein_mass

---
+++ R/protein_mass.R
@@ -0,0 +1,9 @@
+protein_mass <- function(sequence) {
+    amino_acid_mass <- c(
+            A = 71.04, R = 156.10, N = 114.04, D = 115.03, C = 103.01,
+            E = 129.04, Q = 128.06, G = 57.02, H = 137.06, I = 113.08,
+            L = 113.08, K = 128.09, M = 131.04, F = 147.07, P = 97.05,
+            S = 87.03, T = 101.05, W = 186.08, Y = 163.06, V = 99.07)
+    mass <- sum(amino_acid_mass[strsplit(sequence, "")[[1]]], na.rm = TRUE)
+    return(mass)
+    }
