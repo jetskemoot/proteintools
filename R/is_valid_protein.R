4e991d2283bf7d3c7833efb1a243736a81417332
jetskemoot <jetskemoot@users.noreply.github.com>
2025-05-21 18:19:25 +0200

    adjustments

--- R/is_valid_protein.R
+++ R/is_valid_protein.R
@@ -1,3 +1,15 @@
+#' Title Is rotein Sequence is valid?
+#'
+#' @param sequence A character string representing a protein sequence
+#'
+#' @returns A logical value: `TRUE` if all characters are valid amino acids,
+#' `FALSE` otherwise.
+#' @export
+#'
+#' @examples
+#' is_valid_protein("ACDEFGHIKLMNPQRSTVWY")  # TRUE
+#' is_valid_protein("ACDEX")                 # FALSE
+#'
 is_valid_protein <- function(sequence) {
   valid_aa <- c("A","R","N","D","C","E","Q",
                 "G","H","I","L","K","M","F",
